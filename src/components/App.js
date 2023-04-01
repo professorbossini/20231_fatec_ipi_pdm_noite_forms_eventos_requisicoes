6ec96aececc5cfdae961f29b02420395a03e7b08
Rodrigo Bossini <professorbossini@gmail.com>
2023-03-31 22:07:12 -0300

    apostila07(forms-eventos-requisicoes): fim da apostila

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,68 @@
 import Busca from "./Busca"
 import React from 'react'
 import env from 'react-dotenv'
+// import { createClient } from "pexels"
+import ListaImagens from "./ListaImagens"
+import PexelsLogo from "./PexelsLogo"
+import pexelsClient from "../utils/pexelsClient"
 
-class App extends React.Component{
 // const App = () => {
+class App extends React.Component{
+  
+  
+  state = {
+    pics: []
+  }
+  
+  // componentDidMount(){
+  //   this.pexelsClient = createClient(env.PEXELS_KEY)
+  // }
+  
+  // pexelsClient = null
+  // onBuscaRealizada = (termo) => {
+  //   this.pexelsClient.photos.search({
+  //     query: termo
+  //   })
+  //   .then(pics => this.setState({pics: pics.photos}))
+  // }
   onBuscaRealizada = (termo) => {
-    console.log(termo)
+    pexelsClient.get('/search', {
+      params: { query: termo, per_page: 80}
+    })
+    .then(result => this.setState({pics: result.data.photos}))
   }  
   render(){
-    console.log(env.PEXELS_KEY)
-    console.log(window.env.PEXELS_KEY)
     return(
       // div>h1{Exibir uma lista de...}
       <div className="grid justify-content-center m-auto w-9 border-round border-1 border-400">
+        <div className="col-12">
+          <PexelsLogo />
+        </div>
         {/* .col-12 */}
         <div className="col-12">
           <h1>Exibir uma lista de...</h1>
         </div>
         {/* .col-8 */}
-        <div className="col-8">
+        <div className="col-12">
           <Busca onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
+        {/* http://pexels.com/foto1.png => <img src="..." 
+            http://pexels.com/foto2.png => <img src="..."
+        */}
+        <div className="col-12">
+          {/* {
+            this.state.pics.map(pic =>(
+              <div key={pic.id}>
+                <img src={pic.src.small} alt={pic.alt} />
+              </div>
+            ))
+          } */}
+          <div className="grid">
+            <ListaImagens 
+              pics={this.state.pics}
+              // col-md-6
+              imgStyle={'col-12 md:col-6 lg:col-4 xl:col-3'}/>
+          </div>
         </div>
       </div> 
     )
---
+++ src/components/PexelsLogo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PexelsLogo = () => {
+  return (
+    <div>
+      {/* a[href="https://www.pexels.com"][target="_blank"] */}
+      <a href="https://www.pexels.com" target="_blank">
+        {/* img[width=75][src=https://images.pexels.com/lib/api/pexels.png] */}
+        <img src="https://images.pexels.com/lib/api/pexels.png" width={75} />
+
+      </a>
+
+    </div>
+  )
+}
+
+export default PexelsLogo
